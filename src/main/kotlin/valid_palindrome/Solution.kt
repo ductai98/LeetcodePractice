6f7769165b98b2c28667df60f5cc86af38415a0c
ductai <lductai98@gmail.com>
2024-09-16 09:07:13 +0700

    Leetcode 125: Valid Palindrome

---
+++ src/main/kotlin/valid_palindrome/Solution.kt
@@ -0,0 +1,18 @@
+package org.example.valid_palindrome
+
+
+fun main() {
+    val str1 = "A man, a plan, a canal: Panama"
+    val solution = Solution()
+    println(solution.isPalindrome(str1))
+}
+
+class Solution {
+    fun isPalindrome(s: String): Boolean {
+        val str = s.replace("[^A-Za-z0-9]".toRegex(), "").lowercase()
+
+        val reverse = str.reversed()
+
+        return str == reverse
+    }
+}
