c93c5e59eafa5c6cf991e748acbb0d886bab7ccc
ductai <lductai98@gmail.com>
2024-09-17 09:31:47 +0700

    Leetcode 11: Container With Most Water

---
+++ src/main/kotlin/container_with_most_water/Solution.kt
@@ -0,0 +1,31 @@
+package org.example.container_with_most_water
+
+import kotlin.math.max
+import kotlin.math.min
+
+fun main() {
+    val arr = intArrayOf(1,1)
+    val solution = Solution()
+    println(solution.maxArea(arr))
+}
+
+class Solution {
+    fun maxArea(height: IntArray): Int {
+        var result = 0
+        var start = 0
+        var end = height.lastIndex
+
+        while (start < end) {
+            val distance = end - start
+            val area = distance * min(height[start] ,height[end])
+            result = max(area, result)
+            if (height[start] < height[end]) {
+                start++
+            } else {
+                end--
+            }
+        }
+
+        return result
+    }
+}
