88855f823009f8505e2ed7bf61918a219ed30523
ductai <lductai98@gmail.com>
2024-09-18 15:14:11 +0700

    Leetcode 209: Minimum Size Subarray Sum

---
+++ src/main/kotlin/mininum_size_subarray_sum/Solution.kt
@@ -0,0 +1,30 @@
+package org.example.mininum_size_subarray_sum
+
+import kotlin.math.min
+
+fun main() {
+    val arr = intArrayOf(2)
+    val solution = Solution()
+    println(solution.minSubArrayLen(2, arr))
+}
+
+class Solution {
+    fun minSubArrayLen(target: Int, nums: IntArray): Int {
+        var start = 0
+        var end = 0
+        var sum = 0
+        var minium = Int.MAX_VALUE
+        //if (nums.size == 1 && nums[0] == target) return 1
+        while (end < nums.size) {
+            sum += nums[end]
+            while (sum >= target) {
+                minium = min(minium, end - start +1)
+                sum = sum - nums[start]
+                start++
+            }
+            end++
+        }
+
+        return if (minium == Int.MAX_VALUE) 0 else minium
+    }
+}
