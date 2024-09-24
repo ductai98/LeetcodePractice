d404000daa622fa850cc1b0310e0c16d4913650b
ductai <lductai98@gmail.com>
2024-09-24 19:09:52 +0700

    Leetcode 290: Word pattern

---
+++ src/main/kotlin/word_pattern/Solution.kt
@@ -0,0 +1,31 @@
+package org.example.word_pattern
+
+fun main() {
+    val str1 = "aaa"
+    val str2 = "aa aa aa aa"
+    val solution = Solution()
+    println(solution.wordPattern(str1, str2))
+}
+
+class Solution {
+    fun wordPattern(pattern: String, s: String): Boolean {
+        val mapS = mutableMapOf<Char, String>()
+        val mapT = mutableMapOf<String, Char>()
+        val str = s.split("\\s+".toRegex())
+
+        if (pattern.length != str.size) return false
+
+        for (i in 0 until pattern.length) {
+            if (mapS[pattern[i]] != null && mapS[pattern[i]] != str[i]) {
+                return false
+            }
+            if (mapT[str[i]] != null && mapT[str[i]] != pattern[i]){
+                return false
+            }
+            mapS.put(pattern[i], str[i])
+            mapT.put(str[i], pattern[i])
+        }
+
+        return true
+    }
+}
