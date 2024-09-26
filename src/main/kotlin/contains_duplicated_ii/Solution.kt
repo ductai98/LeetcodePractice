5ee3ddfb1abec82228382f8e8031b843b739f46a
ductai <lductai98@gmail.com>
2024-09-26 18:57:04 +0700

    Leetcode 219: Contains Duplicate II

---
+++ src/main/kotlin/contains_duplicated_ii/Solution.kt
@@ -0,0 +1,24 @@
+package org.example.contains_duplicated_ii
+
+import kotlin.math.abs
+
+fun main() {
+    val arr = intArrayOf(1,2,3,1,2,3)
+    val solution = Solution()
+    println(solution.containsNearbyDuplicate(arr, 2))
+}
+
+class Solution {
+    fun containsNearbyDuplicate(nums: IntArray, k: Int): Boolean {
+        val map = mutableMapOf<Int, Int>()
+
+        for (i in 0 until nums.size){
+            if (nums[i] in map &&  abs(map[nums[i]]!! - i) <= k) {
+                return true
+            }
+            map[nums[i]]=i
+        }
+
+        return false
+    }
+}
