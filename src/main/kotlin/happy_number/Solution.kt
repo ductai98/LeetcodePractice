b72230e1360f4b512579d9be9429d275186dc04f
ductai <lductai98@gmail.com>
2024-09-26 15:10:04 +0700

    Leetcode 202: Happy Number

---
+++ src/main/kotlin/happy_number/Solution.kt
@@ -0,0 +1,28 @@
+package org.example.happy_number
+
+
+fun main() {
+    val solution = Solution()
+    println(solution.isHappy(2))
+}
+
+class Solution {
+    fun isHappy(n: Int): Boolean {
+        var str = n.toString()
+        var result = 0
+        val arr = mutableListOf<Int>()
+        while (result != 1) {
+            result = 0
+            for (i in 0 until str.length) {
+                result += (str[i].digitToInt() * str[i].digitToInt())
+            }
+            if (arr.contains(result)) {
+                return false
+            } else {
+                arr.add(result)
+            }
+            str = result.toString()
+        }
+        return true
+    }
+}
