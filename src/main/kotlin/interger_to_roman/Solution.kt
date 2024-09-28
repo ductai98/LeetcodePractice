1b18f372f377a8c943a3ed9b3d9fae69bf41e746
ductai <lductai98@gmail.com>
2024-09-28 07:34:44 +0700

    Leetcode 12: Integer to Roman

---
+++ src/main/kotlin/interger_to_roman/Solution.kt
@@ -0,0 +1,23 @@
+package org.example.interger_to_roman
+
+class Solution {
+    fun intToRoman(num: Int): String {
+        val romanMap = listOf(
+            1000 to "M", 900 to "CM", 500 to "D", 400 to "CD",
+            100 to "C", 90 to "XC", 50 to "L", 40 to "XL",
+            10 to "X", 9 to "IX", 5 to "V", 4 to "IV", 1 to "I"
+        )
+
+        var number = num
+        val result = StringBuilder()
+
+        romanMap.forEach {
+            while (number >= it.first) {
+                result.append(it.second)
+                number -= it.first
+            }
+        }
+
+        return result.toString()
+    }
+}
