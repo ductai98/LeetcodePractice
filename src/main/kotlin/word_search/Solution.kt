08fc9543adcf409ff9c05428c08e44c9946254f8
ductai <lductai98@gmail.com>
2024-10-27 15:06:01 +0700

    Leetcode 79. Word Search

--- src/main/kotlin/word_search/Solution.kt
+++ src/main/kotlin/word_search/Solution.kt
@@ -3,22 +3,17 @@ package org.example.word_search
 
 fun main() {
     val board = arrayOf(
-        charArrayOf('A', 'A', 'A', 'A', 'A', 'A'),
-        charArrayOf('A', 'A', 'A', 'A', 'A', 'A'),
-        charArrayOf('A', 'A', 'A', 'A', 'A', 'A'),
-        charArrayOf('A', 'A', 'A', 'A', 'A', 'A'),
-        charArrayOf('A', 'A', 'A', 'A', 'A', 'B'),
-        charArrayOf('A', 'A', 'A', 'A', 'B', 'A'),
+        charArrayOf('A', 'B', 'C', 'E'),
+        charArrayOf('S', 'F', 'C', 'S'),
+        charArrayOf('A', 'D', 'E', 'E'),
     )
-    println(Solution().exist(board, "AAAAAAAAAAAAABB"))
+    println(Solution().exist(board, "SEE"))
 }
 
 class Solution {
     fun exist(board: Array<CharArray>, word: String): Boolean {
         val rows = board.size
         val cols = board[0].size
-        var final = false
-        val path = mutableListOf<Char>()
         val direction = listOf(
             1 to 0,
             -1 to 0,
@@ -26,32 +21,35 @@ class Solution {
             0 to -1
         )
 
-        fun backtrack(r: Int, c: Int, step: Int) {
+        fun backtrack(r: Int, c: Int, step: Int) : Boolean {
             if (step == word.length) {
-                final = true
-                return
+                return true
             }
             if (r >= rows || c >= cols || r < 0 || c < 0 || board[r][c] == '*' || board[r][c] != word[step]) {
-                return
+                return false
             }
 
             direction.forEach {
                 if (board[r][c] == word[step]) {
-                    path.add(board[r][c])
+                    val temp = board[r][c]
                     board[r][c] = '*'
-                    backtrack(r + it.first, c + it.second, step + 1)
-                    board[r][c] = path.removeLast()
+                    if (backtrack(r + it.first, c + it.second, step + 1)) {
+                        return true
+                    }
+                    board[r][c] = temp
                 }
             }
+
+            return false
         }
 
         for (i in 0 until rows) {
             for (j in 0 until cols) {
-                if (board[i][j] == word[0]) {
-                    backtrack(i, j, 0)
+                if (board[i][j] == word[0] && backtrack(i, j, 0)) {
+                    return true
                 }
             }
         }
-        return final
+        return false
     }
 }
