b5e3f02823117811af98eb96aade96fa5e66c4d3
ductai <lductai98@gmail.com>
2024-10-31 09:12:28 +0700

    Leetcode 74. Search a 2D Matrix

--- src/main/kotlin/search_insert_position/Solution.kt
+++ src/main/kotlin/search_insert_position/Solution.kt
@@ -9,7 +9,6 @@ class Solution {
     fun searchInsert(nums: IntArray, target: Int): Int {
         var start = 0
         var end = nums.size - 1
-        var position = -1
         while (start <= end) {
             val mid = (start + end) / 2
 
@@ -24,14 +23,6 @@ class Solution {
             }
         }
 
-        for (i in 0 until nums.size) {
-            if (nums[i] > target) {
-                return i
-            }
-        }
-
-        if (position == -1) return nums.size
-
-        return position
+        return start
     }
 }
---
+++ src/main/kotlin/search_matrix/Solution.kt
@@ -0,0 +1,52 @@
+package org.example.search_matrix
+
+fun main() {
+    val array = arrayOf(
+        intArrayOf(1)
+    )
+
+    println(Solution().searchMatrix(array, 1))
+}
+
+class Solution {
+    fun searchMatrix(matrix: Array<IntArray>, target: Int): Boolean {
+        var start = 0
+        var end = matrix.size
+        val rows = matrix.size
+        val cols = matrix[0].size
+
+        while (start <= end) {
+            val mid = (start + end) / 2
+            if (mid >= rows) {
+                return false
+            }
+            if (target > matrix[mid][cols -1 ]) {
+                start = mid + 1
+            }else if (target < matrix[mid][0]) {
+                end = mid - 1
+            } else {
+                return searchRow(matrix, target, mid)
+            }
+        }
+        return false
+    }
+
+    fun searchRow(matrix: Array<IntArray>, target: Int, row: Int): Boolean {
+        var start = 0
+        var end = matrix[0].size
+
+        while (start <= end) {
+            val mid = (start + end) / 2
+            val value = matrix[row][mid]
+            if (value == target) {
+                return true
+            }
+            if (value > target) {
+                end = mid - 1
+            } else {
+                start = mid + 1
+            }
+        }
+        return false
+    }
+}
