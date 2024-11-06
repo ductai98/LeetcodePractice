557a34338b3e8c535847f02974dfa1803995b0f7
ductai <lductai98@gmail.com>
2024-11-06 14:40:58 +0700

    Leetcode 136. Single Number

---
+++ src/main/kotlin/single_number/Solution.kt
@@ -0,0 +1,17 @@
+package org.example.single_number
+
+fun main() {
+    val array = intArrayOf(4,2,2,1,1)
+    println(Solution().singleNumber(array))
+}
+
+class Solution {
+    fun singleNumber(nums: IntArray): Int {
+        var result = 0
+        for (num in nums) {
+            result = result xor num
+        }
+
+        return result
+    }
+}
